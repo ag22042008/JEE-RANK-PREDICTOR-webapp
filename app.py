b006095d51d2af16bffdfa7f0b9cdb89ec7cd8fe
Aditya Gupta <coonect2adityagupta@gmail.com>
2026-04-07 07:18:42 +0530

    Make app port configurable via environment variable
    Updated app to run on configurable port from environment variable.

--- app.py
+++ app.py
@@ -66,5 +66,8 @@ def heatmap():
     return jsonify(predictor.get_heatmap_data())
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
